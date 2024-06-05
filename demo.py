875f6ffd6d22b750ce4fee841d0d10b012fa150b
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2024-06-05 21:09:07 +0530

    demo.py

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ c=7j
 d="Anjaleena Sarah"
 e=' Kopraparambil Royden'
 print("Sum : ", a+b) 
-print("The type of a", type(a))  
+print("The type of a", type(a))   
 print("The type of b", type(b)) 
 print("The type of c", type(c))   
 print("The type of d", type(d))  
