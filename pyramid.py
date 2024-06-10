c37fabe48b19a9ecdceb98224bed310810880a55
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2024-06-10 21:27:18 +0530

    pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -1,6 +1,7 @@
 #create a pyramid of any pattern 
-# rows = int(input("Enter number of rows: "))
 
+
+# rows = int(input("Enter number of rows: "))
 # for i in range(rows):
 #     for j in range(i+1):
 #         # print("* ", end=" ")
