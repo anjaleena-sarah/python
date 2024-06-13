10433b4cf63697230ca3779de3bbf50f9da6d581
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2024-06-13 20:28:42 +0530

    Add files via upload

---
+++ python_lab_programs/conopern.py
@@ -0,0 +1,32 @@
+#creating a list
+myList=['albin','bhagya','anjaleena','ridha','abhinav']
+
+print(myList)
+
+print(myList[0])
+
+length=len(myList)
+
+#if-else
+if (length>0):
+    print("List is not empty")
+else:
+    print("empty")
+  
+#for loop  
+# for i in myList:
+#     print (myList)
+
+#sorting the list
+myList.sort()
+print(myList)
+
+#while loop
+i=0
+while i < len(myList):
+  print(myList[i])
+  i = i + 1
+
+
+
+
