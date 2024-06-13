df0a4eaf5ee40823695d05c83254f93927512d0b
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2024-06-13 20:28:07 +0530

    Add files via upload

---
+++ python_lab_programs/polymorphism.py
@@ -0,0 +1,20 @@
+class Animal:
+    def speak(self):
+        pass
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow!"
+
+def animal_sound(animal):
+    print(animal.speak())
+
+dog = Dog()
+cat = Cat()
+
+animal_sound(dog)  
+animal_sound(cat)
