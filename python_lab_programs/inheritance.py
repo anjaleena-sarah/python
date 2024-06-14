ebe7f2c249f5a4d4b0e9baec85a4133c3339e13f
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2024-06-14 20:49:05 +0530

    Add files via upload

---
+++ python_lab_programs/inheritance.py
@@ -0,0 +1,16 @@
+# inheritance
+class CS:
+    def __init__(self, course, subject):
+        self.coursename = course
+        self.subjectname = subject
+
+    def printname(self):
+        print(self.coursename, self.subjectname)
+
+class Commerce(CS):
+    def __init__(self, course, subject):
+        super().__init__(course, subject) 
+        # CS.__init__(self, course, subject)
+
+x = Commerce("MCA", "Python")
+x.printname()
