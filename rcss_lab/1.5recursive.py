eec9ce3fe476acaf907d9e462e57defe6a8c0298
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2024-06-23 10:56:41 +0530

    1.5recursive.py

--- rcss_lab/1.5recursive.py
+++ rcss_lab/1.5recursive.py
@@ -3,5 +3,5 @@ def sumofnumbers(n):
      return 0
     else:
         return n + sumofnumbers(n-1)
-result = sumofnumbers(10)
+result = sumofnumbers(10) 
 print(f"Sum of numbers from 0 to 10: {result}")
