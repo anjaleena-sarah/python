92bb13a9b750d34a47e7a6598870821159cb6a07
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2024-06-25 20:20:18 +0530

    1.8factorial.py

--- rcss_lab/1.8factorial.py
+++ rcss_lab/1.8factorial.py
@@ -4,7 +4,7 @@ def factorial(n):
     # Check if the factorial is already calculated and stored in the dictionary
     if n in factorials:
         return factorials[n]
-
+ 
     # Calculate the factorial if it's not in the dictionary
     if n == 0:
         result = 1
