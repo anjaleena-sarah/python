e5c41613a614032f4608938923b8890a0e9b3478
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2024-07-06 23:18:32 +0530

    1.11copy.py

--- rcss_lab/1.11copy.py
+++ rcss_lab/1.11copy.py
@@ -1,7 +1,7 @@
 def copy_file_line_by_line(source_file, destination_file):
     try:
         with open(source_file, 'r') as src:
-            with open(destination_file, 'w') as dst:
+            with open(destination_file, 'w') as dst: 
                 for line in src:
                     dst.write(line)
         print(f"Contents of {source_file} have been copied to {destination_file}.")
