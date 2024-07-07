812ef012715caec41fd9ff8b9c47b1c0e605b0f5
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2024-07-07 10:59:18 +0530

    1.6reverse.py

--- rcss_lab/1.6reverse.py
+++ rcss_lab/1.6reverse.py
@@ -3,7 +3,7 @@ def is_palindrome(num):
     Checks if a number is a palindrome.
     """
     original_num = num
-    reversed_num = 0
+    reversed_num = 0 
     
     while num > 0:
         digit = num % 10 
