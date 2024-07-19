ed309ccb6bb87e6274fc9556150393e04e196c9e
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2024-07-19 07:11:16 +0530

    1.9set.py

--- rcss_lab/1.9set.py
+++ rcss_lab/1.9set.py
@@ -5,7 +5,7 @@ def set_intersection(set1, set2):
     return set1.intersection(set2) 
  
 def set_difference(set1, set2): 
-    return set1.difference(set2) 
+    return set1.difference(set2)  
  
 # Example usage:
 if __name__ == "__main__":
