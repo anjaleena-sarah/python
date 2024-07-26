fdaa26d5be141690609fa02c30c8c287d95f1eb8
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2024-07-27 00:16:43 +0530

    1.6reverse.py

--- rcss_lab/1.6reverse.py
+++ rcss_lab/1.6reverse.py
@@ -5,7 +5,7 @@ def is_palindrome(num):
     original_num = num 
     reversed_num = 0 
     
-    while num > 0:
+    while num > 0: 
         digit = num % 10 
         reversed_num = reversed_num * 10 + digit
         num //= 10
