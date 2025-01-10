50530ef843d1790827f25d2d166d1092119c661c
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2025-01-10 12:14:04 +0530

    Add files via upload

---
+++ python_lab_programs/numb.py
@@ -0,0 +1,16 @@
+#numbers - int, float, complex
+
+#Sum of 2 numbers
+a=10
+b=20
+print("Sum =",a+b)
+
+
+#Type Casting 
+#float to int
+c=12.5
+print("Value of c =",int(c))
+
+#int to float
+d=12
+print("Value of d =",float(d))
