cb6af0dbe11a8ec77e216f97ad7ea4c0d8705584
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2025-01-15 11:17:41 +0530

    Add files via upload

---
+++ python_lab_programs/basics.py
@@ -0,0 +1,24 @@
+print("hello")
+print(100) #no need of quotes since it is an integer
+print(100+200) #displays 300
+print('hi',"hello", "how are you", 200) #can give more than 2 inputs using comma, can give integer values with string
+
+# integer
+age=22 
+
+# string
+name='sarah'
+name1="magi"
+print(name)
+
+# float 
+marks=78.6
+
+# boolean 
+indian=True
+
+# to print type of the variable 
+print(type(age))
+print(type(name))
+print(type(marks))
+print(type(indian))
